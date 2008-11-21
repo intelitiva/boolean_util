f7c5e79f9d2ee2b2d07953ee2122200cc128975f
vicentemundim <vicente.mundim@gmail.com>
2008-11-21 15:55:07 -0200

    Improving the code so that it does not use a constant to hold boolean values

--- lib/boolean_util.rb
+++ lib/boolean_util.rb
@@ -1,8 +1,6 @@
 # Add some useful methods to work with booleans in general like: 'true', 1 or true
 #
 class BooleanUtil
-  DEFAULT_BOOLEAN_VALUES = { :positives => [true, "true", 1], :negatives => [false, "false", 0] }
-
   # Checks if the given argument is positive.
   #
   def self.true?(arg)
@@ -12,25 +10,25 @@ class BooleanUtil
   # Parse an available value to boolean.
   #
   def self.to_b(arg)
-    all.include?(prepared(arg)) ? DEFAULT_BOOLEAN_VALUES[:positives].include?(prepared(arg)) : (!!arg)
+    self.all.include?(prepared(arg)) ? self.positives.include?(prepared(arg)) : (!!arg)
   end
 
   # Returns all positive values available.
   #
   def self.positives
-    DEFAULT_BOOLEAN_VALUES[:positives]
+    [true, "true", 1]
   end
 
   # Returns all negative values available.
   #
   def self.negatives
-    DEFAULT_BOOLEAN_VALUES[:negatives]
+    [false, "false", 0]
   end
 
   # Returns all values available.
   #
   def self.all
-    a = DEFAULT_BOOLEAN_VALUES.values; return (a[0] + a[1])
+    self.positives + self.negatives
   end
 
   private
@@ -46,3 +44,4 @@ Object.class_eval do
     BooleanUtil.to_b(self)
   end
 end
+
